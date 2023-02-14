e35f6390de07c9847180fcc8f1570b515e34ef55
Thiago da Costa Pinto Frazao de Sousa <f514189@corp.caixa.gov.br>
2023-02-14 20:47:32 -0300

    exemplo endpoint post

---
+++ src/main/java/io/github/tfrazzao/user/UserController.java
@@ -0,0 +1,22 @@
+package io.github.tfrazzao.user;
+
+import io.github.tfrazzao.user.modelos.CriarUsuarioRequestBody;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class UserController {
+
+    @POST
+    public Response criarUsuario(CriarUsuarioRequestBody requestBody) {
+        return Response.status(Response.Status.CREATED).entity(requestBody).build();
+    }
+
+}
---
+++ src/main/java/io/github/tfrazzao/user/modelos/CriarUsuarioRequestBody.java
@@ -0,0 +1,12 @@
+package io.github.tfrazzao.user.modelos;
+
+import lombok.Data;
+
+@Data
+public class CriarUsuarioRequestBody {
+
+    private String nome;
+
+    private Integer idade;
+
+}
