9c19d16a3303d6b88f985e7ce97d208357f58dbb
Thiago da Costa Pinto Frazao de Sousa <f514189@corp.caixa.gov.br>
2023-03-07 21:05:58 -0300

    guardando SQL de criacao da tabela postagens

--- script_db/tables_quarkus_social.sql
+++ script_db/tables_quarkus_social.sql
@@ -2,3 +2,10 @@ create table USUARIO(
 	id bigserial not null primary key, 
 	name varchar(100) not null, 
 	age integer not null);
+
+create table POSTAGENS(
+    id bigserial not null primary key,
+    texto text not null,
+    data timestamp not null,
+    id_usuario bigint not null references USUARIO(id)
+)
